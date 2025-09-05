1acfca31c304dc27a74c474eb22fb3732f9733d8
Razibul-Islam <razibulislam665@gmail.com>
2025-09-05 15:47:18 +0600

    successfully finish testing

--- test/DecentralizedEscrow.t.sol
+++ test/DecentralizedEscrow.t.sol
@@ -12,12 +12,23 @@ contract TestingDecentralizedEscrow is Test {
 
     function setUp() public {
         decentralizedEscrow = new DecentralizedEscrow();
-        vm.deal(user,AMOUNT);
+        vm.deal(user, AMOUNT);
     }
 
-    function testRegisterAsAClient() external {
+    modifier RegClient() {
         vm.prank(user);
         decentralizedEscrow.register(uint8(DecentralizedEscrow.Role.Client));
+        _;
+    }
+    modifier RegFreelancer() {
+        vm.prank(user);
+        decentralizedEscrow.register(
+            uint8(DecentralizedEscrow.Role.Freelancer)
+        );
+        _;
+    }
+
+    function testRegisterAsAClient() external RegClient {
         (
             DecentralizedEscrow.Role role,
             address userAddress,
@@ -29,11 +40,51 @@ contract TestingDecentralizedEscrow is Test {
         assertTrue(isRegister);
     }
 
-    function testRegisterAsFreelancer() external {
+    function testRegisterAsClientExpectRevertWhenSameUserTryToRegister()
+        external
+        RegClient
+    {
+        (
+            DecentralizedEscrow.Role role,
+            address userAddress,
+            bool isRegister
+        ) = decentralizedEscrow.users(user);
+
+        assertEq(uint(role), uint(DecentralizedEscrow.Role.Client));
+        assertEq(userAddress, user);
+        assertTrue(isRegister);
+
         vm.prank(user);
+        vm.expectRevert(
+            DecentralizedEscrow.Client__AlreadeyHaveAccount.selector
+        );
+        decentralizedEscrow.register(uint8(DecentralizedEscrow.Role.Client));
+    }
+
+    function testRegisterAsFreelancerExpectRevertWhenSameUserTryToRegister()
+        external
+        RegFreelancer
+    {
+        (
+            DecentralizedEscrow.Role role,
+            address userAddress,
+            bool isRegister
+        ) = decentralizedEscrow.users(user);
+
+        assertEq(uint(role), uint(DecentralizedEscrow.Role.Freelancer));
+        assertEq(userAddress, user);
+        assertTrue(isRegister);
+
+        vm.prank(user);
+        vm.expectRevert(
+            DecentralizedEscrow.Freelancer__AlreadeyHaveAccount.selector
+        );
         decentralizedEscrow.register(
             uint8(DecentralizedEscrow.Role.Freelancer)
         );
+    }
+
+    function testRegisterAsFreelancer() external RegFreelancer {
         (
             DecentralizedEscrow.Role role,
             address userAddress,
@@ -45,10 +96,7 @@ contract TestingDecentralizedEscrow is Test {
         assertTrue(isRegister);
     }
 
-    function testcheckClientRoleOnlyCanCreateProject() external {
-        vm.prank(user);
-        decentralizedEscrow.register(uint8(DecentralizedEscrow.Role.Client));
-
+    function testcheckClientRoleOnlyCanCreateProject() external RegClient {
         uint256 deadline = block.timestamp + 7 days;
 
         vm.prank(user);
@@ -66,17 +114,100 @@ contract TestingDecentralizedEscrow is Test {
 
         assertEq(client, user);
         assertEq(amount, 1 ether);
-        assertEq(uint256(status), uint256(DecentralizedEscrow.ProjectStatus.Open));
+        assertEq(
+            uint256(status),
+            uint256(DecentralizedEscrow.ProjectStatus.Open)
+        );
         assertEq(deadline, deadlines);
     }
 
-    function testcheckFreelancerRoleCallItFail() external {
-        vm.prank(user);
-        decentralizedEscrow.register(uint8(DecentralizedEscrow.Role.Freelancer));
+    function testcheckFreelancerRoleCallItFail() external RegFreelancer {
         uint256 deadline = block.timestamp + 7 days;
 
         vm.prank(user);
         vm.expectRevert();
         decentralizedEscrow.createProject{value: 1 ether}(1 ether, deadline);
+    }
+
+    function testSubmitProjectRevertWhenIdDosentExit() external RegFreelancer {
+        vm.prank(user);
+        vm.expectRevert();
+        decentralizedEscrow.submitProject(5);
+    }
+
+    function testExpectRevertWhenCheckNotCurrectFreelancer() external {
+        address freelancerA = makeAddr("FreelancerA");
+        address freelancerB = makeAddr("FreelancerB");
+        uint256 deadline = block.timestamp + 7 days;
+
+        vm.prank(freelancerA);
+        decentralizedEscrow.register(
+            uint8(DecentralizedEscrow.Role.Freelancer)
+        );
+
+        vm.prank(freelancerB);
+        decentralizedEscrow.register(
+            uint8(DecentralizedEscrow.Role.Freelancer)
+        );
+
+        vm.prank(user);
+        decentralizedEscrow.createProject{value: 0.5 ether}(
+            0.5 ether,
+            deadline
+        );
+
+        vm.prank(freelancerB);
+        vm.expectRevert();
+        decentralizedEscrow.submitProject(1);
+    }
+
+    function testApproveProjectWhenEverythingIsOkay() external RegClient {
+        address freelancer = makeAddr("Freelancer");
+        uint256 deadline = block.timestamp + 7 days;
+
+        vm.prank(freelancer);
+        decentralizedEscrow.register(
+            uint8(DecentralizedEscrow.Role.Freelancer)
+        );
+
+        vm.prank(user);
+        decentralizedEscrow.createProject{value: 0.5 ether}(
+            0.5 ether,
+            deadline
+        );
+
+        vm.prank(freelancer);
+        decentralizedEscrow.acceptProject(1);
+
+        vm.prank(freelancer);
+        decentralizedEscrow.submitProject(1);
+
+        vm.prank(user);
+        decentralizedEscrow.approveProject(1);
+    }
+
+    function testRejectProject() external RegClient {
+        address freelancer = makeAddr("Freelancer");
+        uint256 deadline = block.timestamp + 7 days;
+
+        vm.prank(freelancer);
+        decentralizedEscrow.register(
+            uint8(DecentralizedEscrow.Role.Freelancer)
+        );
+
+        vm.prank(user);
+        decentralizedEscrow.createProject{value: 0.5 ether}(
+            0.5 ether,
+            deadline
+        );
+
+        vm.prank(freelancer);
+        decentralizedEscrow.acceptProject(1);
+
+        vm.prank(freelancer);
+        decentralizedEscrow.submitProject(1);
+
+        vm.prank(user);
+        decentralizedEscrow.rejectProject(1);
     }
 }
